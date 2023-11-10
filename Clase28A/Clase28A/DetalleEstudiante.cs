40365506a49502eeda0bc4343afb3ec04c17f303
Iri <89229099+IAgretti@users.noreply.github.com>
2023-11-10 18:29:03 -0300

    Ejercicio DetalleEstudiante
    Ejercicio propuesto con Interfaz y Clases

---
+++ Clase28A/Clase28A/DetalleEstudiante.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase28A
+{
+    public class DetalleEstudiante
+    {
+        public int anio;
+        public string nombre;
+
+        public void Detalle(int a, string b)
+        {
+            anio = a;
+            nombre = b;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("El nombre del estudiante es: " + nombre + " y el anio es: " + anio);
+        }
+    }
+    public interface ISports
+    {
+        void Deporte(string a);
+        void VerDeporte();
+    }
+    public class Sport: DetalleEstudiante, ISports
+    {
+        public string nomDeporte;
+        public void Deporte(string a)
+        {
+            nomDeporte = a;
+        }
+        public void VerDeporte()
+        {
+            Console.WriteLine("El deporte es: " + nomDeporte);
+        }
+        public void VerTodo()
+        {
+            Mostrar();
+            VerDeporte();
+        }
+    }
+}
+
---
+++ Clase28A/Clase28A/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Clase28A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport de = new Sport();
+            de.Detalle(2023, "Luis");
+            de.Deporte("Voley");
+            de.VerTodo();
+            Console.ReadKey();
+
+        }
+    }
+}
