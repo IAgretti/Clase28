059ed5bbb6f76d01b77846835b8596ca58a528bd
Iri <89229099+IAgretti@users.noreply.github.com>
2023-11-10 18:30:20 -0300

    Ejercicio ITransacciones

---
+++ Clase28/Clase28/ITransacciones.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase28
+{
+    public interface ITransacciones
+    {
+        void ShowTran();
+        double MontoTotal();
+    }
+    public class Transaccion:ITransacciones
+    {
+        private string tCode, date;
+        private double monto;
+
+        public Transaccion()
+        {
+            tCode = "";
+            date = "";
+            monto = 0.0;
+        }
+        public Transaccion(string c, string d, double a)
+        {
+            tCode = c;
+            date = d;
+            monto = a;
+        }
+        public double MontoTotal()
+        {
+            return monto;
+        } 
+        public void ShowTran()
+        {
+            Console.WriteLine("Transacción: " + tCode);
+            Console.WriteLine("Fecha: " + date);
+            Console.WriteLine("Monto: " + MontoTotal() + "\n");
+        }
+    }
+    
+}
---
+++ Clase28/Clase28/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Clase28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaccion t1 = new Transaccion("001","8/10/2023", 78900.00);
+            Transaccion t2 = new Transaccion("025", "9/11/2023", 142025.30);
+            Console.Write("Ingrese el código: ");
+            string cod = Console.ReadLine();
+            Console.Write("Ingrese la fecha: ");
+            string fecha = Console.ReadLine();
+            Console.Write("Ingrese el monto: ");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            Transaccion t3 = new Transaccion(cod, fecha, monto);
+
+            t1.ShowTran();
+            t2.ShowTran();
+            t3.ShowTran();
+
+            Console.ReadKey();
+        }
+    }
+}
